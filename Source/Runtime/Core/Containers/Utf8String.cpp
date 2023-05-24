6abf6adee324e5de8b110c89fa2018fb9bc0fc69
Avram Traian <traian.avram1@gmail.com>
2023-05-24 13:52:46 +0300

    Added utility functions for UTF-8 encoded strings

---
+++ Source/Runtime/Core/Containers/Utf8String.cpp
@@ -0,0 +1,154 @@
+// Copyright (c) 2023 Basalt Technology Group. All rights reserved.
+
+#include "Utf8String.h"
+
+namespace Basalt
+{
+
+Usize UTF8Calls::Length(const char* string)
+{
+    Usize length = 0;
+    
+    while (*string)
+    {
+        const U32 codepointWidth = UTF8Calls::BytesToCodepointWidth(string);
+        if (codepointWidth == 0)
+        {
+            return InvalidSize;
+        }
+
+        string += codepointWidth;
+        ++length;
+    }
+
+    return length;
+}
+
+U32 UTF8Calls::CodepointToBytes(UTF8Codepoint codepoint, void* buffer)
+{
+    U8* bytes = (U8*)buffer;
+
+    if (0x0000 <= codepoint && codepoint <= 0x007F)
+    {
+        bytes[0] = (U8)codepoint;
+        return 1;
+    }
+
+    if (0x0080 <= codepoint && codepoint <= 0x07FF)
+    {
+        bytes[0] = ((codepoint >> 6) & 0x1F) | 0xC0;
+        bytes[1] = ((codepoint >> 0) & 0x3F) | 0x80;
+        return 2;
+    }
+
+    if (0x0800 <= codepoint && codepoint <= 0xFFFF)
+    {
+        bytes[0] = ((codepoint >> 12) & 0x0F) | 0xE0;
+        bytes[1] = ((codepoint >> 6)  & 0x3F) | 0x80;
+        bytes[2] = ((codepoint >> 0)  & 0x3F) | 0x80;
+        return 3;
+    }
+
+    if (0x10000 <= codepoint)
+    {
+        bytes[0] = ((codepoint >> 18) & 0x07) | 0xF0;
+        bytes[1] = ((codepoint >> 12) & 0x3F) | 0x80;
+        bytes[2] = ((codepoint >> 6)  & 0x3F) | 0x80;
+        bytes[3] = ((codepoint >> 0)  & 0x3F) | 0x80;
+        return 4;
+    }
+
+    return 0;
+}
+
+UTF8Codepoint UTF8Calls::BytesToCodepoint(const void* buffer, U32* outCodepointWidth /*= nullptr*/)
+{
+    const U8* bytes = (const U8*)buffer;
+
+    if ((bytes[0] & 0x80) == 0x00)
+    {
+        if (outCodepointWidth)
+        {
+            *outCodepointWidth = 1;
+        }
+
+        return bytes[0];
+    }
+
+    if ((bytes[0] & 0xE0) == 0xC0)
+    {
+        if (outCodepointWidth)
+        {
+            *outCodepointWidth = 2;
+        }
+
+        UTF8Codepoint codepoint = 0;
+        codepoint += (bytes[0] & 0x1F) << 6;
+        codepoint += (bytes[1] & 0x3F) << 0;
+        return codepoint;
+    }
+
+    if ((bytes[0] & 0xF0) == 0xE0)
+    {
+        if (outCodepointWidth)
+        {
+            *outCodepointWidth = 3;
+        }
+
+        UTF8Codepoint codepoint = 0;
+        codepoint += (bytes[0] & 0x1F) << 12;
+        codepoint += (bytes[1] & 0x3F) << 6;
+        codepoint += (bytes[2] & 0x3F) << 0;
+        return codepoint;
+    }
+
+    if ((bytes[0] & 0xF8) == 0xF0)
+    {
+        if (outCodepointWidth)
+        {
+            *outCodepointWidth = 4;
+        }
+
+        UTF8Codepoint codepoint = 0;
+        codepoint += (bytes[0] & 0x1F) << 18;
+        codepoint += (bytes[1] & 0x3F) << 12;
+        codepoint += (bytes[2] & 0x3F) << 6;
+        codepoint += (bytes[3] & 0x3F) << 0;
+        return codepoint;
+    }
+
+    if (outCodepointWidth)
+    {
+        *outCodepointWidth = 0;
+    }
+    return InvalidUTF8Codepoint;
+}
+
+U32 UTF8Calls::BytesToCodepointWidth(const void* buffer)
+{
+    const U8 byte = *(const U8*)buffer;
+
+    if ((byte & 0x80) == 0x00)
+    {
+        return 1;
+    }
+
+    if ((byte & 0xE0) == 0xC0)
+    {
+        return 2;
+    }
+
+    if ((byte & 0xF0) == 0xE0)
+    {
+        return 3;
+    }
+
+    if ((byte & 0xF8) == 0xF0)
+    {
+        return 4;
+    }
+
+    return 0;
+}
+
+} // namespace Basalt
---
+++ Source/Runtime/Core/Containers/Utf8String.h
@@ -0,0 +1,71 @@
+// Copyright (c) 2023 Basalt Technology Group. All rights reserved.
+
+#pragma once
+
+#include "Core/CoreDefines.h"
+#include "Core/CoreTypes.h"
+#include "Core/AssertionMacros.h"
+
+namespace Basalt
+{
+
+/**
+ * Primitive type that represents an UTF-8 codepoint.
+ * Because the UTF-8 encoding allows characters of 4-bytes the most, this is basically a `U32`.
+ */
+using UTF8Codepoint = U32;
+
+constexpr UTF8Codepoint InvalidUTF8Codepoint = -1;
+
+/**
+ * Structure that contains all of the utility functions that manipulate UTF-8
+ * encoded strings and codepoints.
+ */
+struct BASALT_API UTF8Calls
+{
+public:
+    /**
+     * Gets the number of codepoints that the string is composed of.
+     * 
+     * @param string The UTF-8 encoded, null-terminated string.
+     * @return The number of codepoints.
+     */
+    static Usize Length(const char* string);
+
+    /**
+     * Converts a codepoint to an UTF-8 encoded sequence of bytes.
+     * 
+     * @param codepoint The codepoint to convert.
+     * @param buffer Pointer to the buffer where the bytes will be written.
+     *               Unless the width of the codepoint is known, this buffer
+     *               should be 4-bytes long.
+     * 
+     * @return The width (in bytes) of the codepoint. If zero is returned, the
+     *         provided codepoint is not valid UTF-8.
+     */
+    static U32 CodepointToBytes(UTF8Codepoint codepoint, void* buffer);
+
+    /**
+     * Converts an UTF-8 encoded byte sequence to a codepoint.
+     * 
+     * @param buffer Pointer to the buffer where the bytes are stored.
+     * @param outCodepointWidth This will be filled with the width of the codepoint.
+     *                          If the byte sequence is not valid UTF-8, this will be
+     *                          set to zero.
+     * 
+     * @return The UTF-8 codepoint. If the byte sequence is not valid, `InvalidUTF8Codepoint`
+      *        will be returned.
+     */
+    static UTF8Codepoint BytesToCodepoint(const void* buffer, U32* outCodepointWidth = nullptr);
+
+    /**
+     * Calculates the width of the codepoint that is represented by a given byte sequence.
+     * If the byte sequence is not valid UTF-8, zero will be returned.
+     * 
+     * @param buffer The UTF-8 encoded byte sequence.
+     * @return The width if the codepoint or zero if the byte sequence is not valid UTF-8.
+     */
+    static U32 BytesToCodepointWidth(const void* buffer);
+};
+
+} // namespace Basalt
