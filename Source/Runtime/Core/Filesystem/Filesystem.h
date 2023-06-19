dd8ae95ee862980149d592b4071235177ab30380
Avram Traian <traian.avram1@gmail.com>
2023-06-19 10:03:42 +0300

    Added documentation for opening files via the filesystem

--- Source/Runtime/Core/Filesystem/Filesystem.h
+++ Source/Runtime/Core/Filesystem/Filesystem.h
@@ -30,21 +30,38 @@ enum class EFilesystemType : U8
     Virtual
 };
 
+/**
+ * Enumeration of error codes that the filesystem uses to propagate errors.
+ */
 enum class EFilesystemError : U32
 {
     /** The filesystem operation was performed successfully. */
     Success = 0,
+
     /** An unknown/unexpected error occurred. */
     Unknown,
+
+    /** The provided file handle is invalid. */
+    InvalidFileHandle,
+
     /** The filesystem couldn't locate the file. Most likely caused by an invalid path. */
     FileNotFound,
+
+    /** The provided handle represents a directory, but a file was expected. */
+    FileIsDirectory,
 };
 
+/**
+ * Structure that describes how the filesystem should work.
+ */
 struct FilesystemDescription
 {
     EFilesystemType type = EFilesystemType::Physical;
 };
 
+/**
+ * Abstract interface that all filesystems, virtual or physical, must provide.
+ */
 class IFilesystem
 {
 public:
@@ -59,8 +76,15 @@ public:
     virtual bool IsInitialized() const = 0;
 
 public:
+    /**
+     * Checks if the given handle represents a valid file in the filesystem.
+     * 
+     * @param file_handle The file handle to check.
+     * @return True if the handle is valid and can be used; False otherwise.
+     */
     virtual bool IsFileHandleValid(FileHandle file_handle) const = 0;
 
+    /** @return The last error code posted by the filesystem implementation. */
     virtual EFilesystemError GetLastErrorCode() const = 0;
 
 public:
@@ -76,9 +100,38 @@ public:
      */
     virtual U64 FileSize(StringView filepath) const = 0;
 
-
+    /**
+     * Opens a file for reading purposes.
+     * 
+     * Error codes:
+     *  - FileNotFound  => The file doesn't exist. Most likely caused by providing a path
+     *                     that doesn't represents a valid file in the filesystem.
+     *  - Unknown       => An unknown error occurred.
+     * 
+     * @param filepath                  The path where the file is located in the filesystem.
+     * @param allow_writing_while_open  Whether or not other processes can access the file
+     *                                  and write to it while it is opened by this process.
+     * 
+     * @return The file handle that represents the opened file. If the handle is invalid,
+     *         an error occurred. Inspect `GetLastErrorCode()` for more details.
+     */
     virtual FileHandle OpenForReading(StringView filepath, bool allow_writing_while_open) = 0;
 
+    /**
+     * Opens a file for writing purposes.
+     * 
+     * Error codes:
+     *  - Unknown => An unknown error occurred.
+     * 
+     * @param filepath                  The path where the file is located in the filesystem.
+     * @param allow_reading_while_open  Whether or not other processes can access the file and
+     *                                  read from it while it is opened by this process.
+     * @param append                    Whether or not to append the bytes to the data that
+     *                                  is currently stored in the file.
+     * 
+     * @return The file handle that represents the opened file. If this handle is invalid, an
+     *         error occured. Inspect `GetLastErrorCode()` for more details.
+     */
     virtual FileHandle OpenForWriting(StringView filepath, bool allow_reading_while_open, bool append) = 0;
 };
 
